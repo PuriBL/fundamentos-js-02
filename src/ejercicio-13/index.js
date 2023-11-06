7cebd3bf82e1f524b946a37427ecf9b04b1d1107
Puri <purificacion.bastida@gmail.com>
2023-11-06 21:53:14 +0100

    Añadir ejercicio 13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,16 @@
-// Escribe tu código aquí:
+// Closures: crea una función llamada contadorClosures que retome otra función. 
+// Esta función interna debe mantener un contador interno y cada vez que se llame, 
+// incrementa el contador y lo devuelve. Utilizar closures para lograr esto.
+
+function contadorClosures () {
+    let contador = 2
+
+    return function () {
+        contador ++ 
+        return contador
+    }
+}
+const incrementar = contadorClosures()
+
+console.log(incrementar()) // 1
+console.log(incrementar()) // 2
