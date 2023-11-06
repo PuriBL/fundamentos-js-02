eae0cb0478869b24e15fe4d79fcb53d85ba0c730
Puri <purificacion.bastida@gmail.com>
2023-11-06 18:15:57 +0100

    Añadir ejercicio 6

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Crea una función llamada saludar que acepte un mombre como parámetro y retorne 
+// un mensaje de saludo:
+
+function saludar(nombre) {
+    return '¡Hola, ' + nombre + '!'
+}
+
+console.log(saludar('Juan'))
+
+
