3e118ef64dfb01b9a5057dd8683155163f087f2e
Puri <purificacion.bastida@gmail.com>
2023-11-06 18:42:16 +0100

    Añadir ejercicio 9

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Funciones anónimas y expresiones de función: crea una función anónima que acepte 
+// dos números como argumentos y retorne su suma.
+
+const suma = function (a, b) {
+    return a + b
+}
+console.log(suma(2 , 6))
+
+
+
