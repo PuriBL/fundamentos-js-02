c33504fed17f19b4c88a5805abf8b77ccf18707f
Puri <purificacion.bastida@gmail.com>
2023-11-06 18:33:26 +0100

    Añadir ejercicio 8

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Retorno de valores: crea una función llamada esMayorDeEdad que acepte un nombre como
+// parámetro y retorne true si la edad es mayor o igual a 18 y false si no lo es:
+
+function esMayorDeEdad(edad) {
+    return edad >= 18
+}
+console.log(esMayorDeEdad(12))
+
+
+
