87552aa9d4ec962353c0f4e1c718faa6468b22d8
Puri <purificacion.bastida@gmail.com>
2023-11-06 18:50:07 +0100

    Añadir ejercicio 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,6 @@
-// Escribe tu código aquí:
+// Arrow Functions: Convierte la función del ejercicio anterior en
+// una arrow function
+
+const sumaArrow = (a, b)=> a + b 
+console.log (sumaArrow(2, 6))
+
