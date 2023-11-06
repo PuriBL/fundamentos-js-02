e35dfedf17672dd62f0735c100dd722d4c9c7b02
Puri <purificacion.bastida@gmail.com>
2023-11-06 18:06:17 +0100

    Añadir ejercicio 5

--- src/ejercicio-05/index.js
+++ src/ejercicio-05/index.js
@@ -1,1 +1,14 @@
-// Escribe tu código aquí:
+// Bucles for... in crea un objeto con algunas propiedades y utiliza un bucle for ... in
+// para imprimir cada propiedad y su valor en la consola.
+
+const persona = {
+    nombre: 'Luis',
+    edad: 30,
+    ciudad: 'Madrid'
+}
+
+for (const propiedad in persona) {
+    console.log(propiedad + ': ' + persona[propiedad])
+}
+
+
