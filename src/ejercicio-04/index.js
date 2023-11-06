c46a2dedaa913d7fb12d245efcb9b2817ea3a667
Puri <purificacion.bastida@gmail.com>
2023-11-06 17:48:58 +0100

    Añadir ejercicio 4

--- src/ejercicio-04/index.js
+++ src/ejercicio-04/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Bucle for... of crea un array de nombres y utiliza un bucle for... of 
+// para imprimir cada nombre en la consola
+
+const nombres = ['Ana', 'Juan', 'María', 'Carlos']
+
+for (const nombre of nombres) {
+    console.log(nombre)
+}
+
